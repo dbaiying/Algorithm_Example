64085b01e7c9a2991832dd57fec26837e494e2ba
dbaiying <WAlhl1314>
2020-12-22 10:48:03 +0800

    commit

--- merge_sort3.py
+++ merge_sort3.py
@@ -1,7 +1,11 @@
+arr = [1, 4, 2, 5, 9, 10, 30, 6]
+
+
 def merge(a, b):
     merged = []
-    i, j = 0, 0
-    while i < len(a) and j < len(b):
+    i = 0
+    j = 0
+    while len(a) > i and len(b) > j:  # 如果while len(a)>0 and len(b)>0,则此式永为真，当使用条件判断时,不能常量与常量比较
         if a[i] <= b[j]:
             merged.append(a[i])
             i += 1
@@ -19,10 +23,9 @@ def merge_sort(c):
     mid = len(c) // 2
     a = merge_sort(c[:mid])
     b = merge_sort(c[mid:])
+    # a = c[:mid]  c[:mid]不是已经拍好序的子数组
+    # b = c[mid:]
     return merge(a, b)
 
 
-A = [1, 4, 2, 5, 9, 10, 30, 6]
-merge_sort(A)
-
-print(merge_sort(A))
+print(merge_sort(arr))
